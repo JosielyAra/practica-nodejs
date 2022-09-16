3f7f8049d12c6d8ec42170aa932a7d6fe3034685
Josi <jtk.jpaf@gmail.com>
2022-09-15 21:56:44 -0400

    Insertando modulos de la web y de nodejs, peticiones y respuestas

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const http = require("http");
+/*const http = require("http"); //Modulo ya instalado con Node Js
+const colors = require('colors');
 
 const handleServer = function (req, res) {
   res.writeHead(200, { "Content-type": "text/html" });
@@ -9,5 +10,19 @@ const handleServer = function (req, res) {
 const server = http.createServer(handleServer);
 
 server.listen(3000, function () {
-  console.log("Server on port 3000");
+  console.log("Server on port 3000".yellow);
+});
+*/
+
+const express = require("express"); //Framework de Node js
+const colors = require("colors"); //Modulo instalado con "npm install colors" desde la pagina npmjs
+const server = express();
+
+server.get("/", function (req, res) {
+  res.send("<h1>Hola Mundo con Express y Node Js</h1>");
+  res.end();
+});
+
+server.listen(3000, function () {
+  console.log("server on port 3000".red);
 });
