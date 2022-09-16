ebe7aca9553f9301c459ff748d90cb3a774646aa
Josi <jtk.jpaf@gmail.com>
2022-09-15 21:15:14 -0400

    modulo File System

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const os = require('os');
+const fs = require("fs");
+/*
 
-console.log(os.platform());
-console.log(os.release());
-console.log('free men', os.freemem(), 'bytes');
-console.log('Total men', os.totalmem(), 'bytes');
+
+fs.writeFile('./texto.txt', 'linea uno', function(err){
+    if(err){
+        console.log(err);
+    }
+    console.log('archivo creado');
+});
+
+console.log('ult lin de cod');
+*/
+
+fs.readFile("./texto.txt", function (err, data) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data.toString());
+});
